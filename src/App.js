455e545ff8949b1204557fd1f57de5d7c26dfe4e
Moosesg10 <moisesgak66@gmail.com>
2023-08-30 00:52:36 -0400

    golasS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import express from "express";
 import cors from "cors"
 import IndexRouter from "./routes/index.routes.js"
 import * as url from 'url';
+import { URL_REQUEST } from "./Config.js";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express()
 
+
 app.use(express.json())
 app.use(cors ({
-    origin:"http://localhost:8080"
+    origin:URL_REQUEST
 }))
 
 app.use(IndexRouter)
--- src/Config.js
+++ src/Config.js
@@ -7,3 +7,4 @@ export const  AWS_BUKECT_NAME = process.env.AWS_BUKECT_NAME
 export const  AWS_BUKECT_REGION = process.env.AWS_BUKECT_REGION
 export const  AWS_BUKECT_PUBLIC_KEY = process.env.AWS_BUKECT_PUBLIC_KEY
 export const  AWS_BUKECT_SECRET_KEY = process.env.AWS_BUKECT_SECRET_KEY
+export const  URL_REQUEST = process.env.URL_REQUEST
